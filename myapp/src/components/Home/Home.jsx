92dd3f6dae07fe1e92f0faee21c940a606e0e975
durlinlin <linlin.lam01@gmail.com>
2022-12-21 03:42:57 -0500

    margin edit on project name

--- myapp/src/components/Home/Home.jsx
+++ myapp/src/components/Home/Home.jsx
@@ -8,7 +8,8 @@ export default function Home(props) {
   return (
     <div className='screen home-box' >
       <h1 style={props.slideH1}>
-        <div className='hello'> Hi, my name is </div> Lin Lin Lam.  
+      <div className='hello'> Hi, my name is </div>
+        Lin Lin Lam.  
       </h1>
       <h3 className='text-one' style={props.slideH3}>I am a Software Engineer.</h3>
       <h3 className='text-two' style={props.slideH3}>Eager to learn , build, and innovate.</h3>
